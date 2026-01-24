f05be8b8a5fb9497c663f42f56d34bc1c4364a58
Jayce Bordelon <b.jayce@wustl.edu>
2026-01-24 16:52:08 -0600

    New Worflow

--- jayceb.com/app/page.tsx
+++ jayceb.com/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/hero";
 
 export default function page() {
   return (
-    <main className="h-screen">
+    <main className="h-dvh overflow-hidden">
       <HeroSection />
     </main>
   );
