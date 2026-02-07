8a3694216faee8607e29025aa04f200fe40b882f
Jayce Bordelon <b.jayce@wustl.edu>
2026-02-06 18:29:29 -0600

    patch:workflow

--- jaycebordelon.com/app/robots.ts
+++ jaycebordelon.com/app/robots.ts
@@ -1,14 +1,13 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-	const siteUrl =
-		process.env.NEXT_PUBLIC_SITE_URL || "https://jaycebordelon.com";
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://jaycebordelon.com";
 
-	return {
-		rules: {
-			userAgent: "*",
-			allow: "/",
-		},
-		sitemap: `${siteUrl}/sitemap.xml`,
-	};
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
 }
